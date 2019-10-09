155ca400107a9e0c36e567243c7d4fe358b67ea4
quietcreek <quietcreek@gmail.com>
2019-10-09 13:49:26 -0400

    .

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
 	char name[255];
 
-	printf("Enter your name: ");
+	printf("Enter first your name: ");
 	fgets(name, 255, stdin);
 	name[strlen(name)-1] = '\0'; /* remove the newline at the end */
 
